d5e66f7330bb818e34b76f88663df9c1b74cbc89
murat akbulut <m.akblt@live.com>
2024-08-09 14:58:40 +0300

    Update request.ts

--- request.ts
+++ request.ts
@@ -1,36 +1,37 @@
-export interface ResponseType extends Response {
-  data?: any;
+export interface ResponseType<T> extends Response {
+  data: T;
 }
-export interface ErrorType extends Error {
-  response?: ResponseType;
+export interface ErrorType<T = any> extends Error {
+  response?: ResponseType<T>;
 }
 
-export async function request(options: any): Promise<ResponseType> {
+export async function request<T = any>(options: any): Promise<ResponseType<T>> {
   if (options.data) {
-		options.headers = { ...options.header, 'Content-Type': 'application/json'}
+    options.headers = { ...options.header, 'Content-Type': 'application/json'}
     options.body = JSON.stringify(options.data);
   }
-  const response: ResponseType = await fetch(options.url, options);
+  let _response = await fetch(options.url, options);
+  let response = Object.assign(_response, { data: {} }) as ResponseType<T>
   try {
-    response.data = await response.json();
+    const data = await _response.json();
+    response = Object.assign(response, { data })
   } catch (error) {}
 
   if (!response.ok) {
-    const error: ErrorType = new Error(response.statusText);
+    const error: ErrorType<T> = new Error(response.statusText);
     error.response = response;
     throw error;
   }
 
-
-  return response as ResponseType;
+  return response;
 }
 
-request.get = (url: string, options?: any) => request({ url, ...options });
-request.post = (url: string, data: any, options: any = {}) =>
-  request({ url, ...options, data, method: 'POST' });
+request.get = <T = any>(url: string, options?: any) => request<T>({ url, ...options });
+request.post = <T = any>(url: string, data: any, options: any = {}) =>
+  request<T>({ url, ...options, data, method: 'POST' });
 
-request.put = (url: string, data: any, options: any = {}) =>
-  request({ url, ...options, data, method: 'PUT' });
+request.put = <T = any>(url: string, data: any, options: any = {}) =>
+  request<T>({ url, ...options, data, method: 'PUT' });
 
-request.delete = (url: string, options: any = {}) =>
-  request({ url, ...options, method: 'DELETE' });
+request.delete = <T = any>(url: string, options: any = {}) =>
+  request<T>({ url, ...options, method: 'DELETE' });
