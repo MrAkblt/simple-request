affae67dcdde6c65061178406fc74b1d6027eaaa
murat akbulut <m.akblt@live.com>
2024-08-10 21:23:25 +0300

    Create macros.ts

---
+++ macros.ts
@@ -0,0 +1,16 @@
+import request from "./request";
+
+const helpers = {
+	get: <T = any>(url: string, options?: any) => request<T>({ url, ...options }),
+
+	post: <T = any>(url: string, data: any, options: any = {}) =>
+		request<T>({ url, ...options, data, method: 'POST' }),
+
+	put: <T = any>(url: string, data: any, options: any = {}) =>
+		request<T>({ url, ...options, data, method: 'PUT' }),
+
+	delete: <T = any>(url: string, options: any = {}) =>
+		request<T>({ url, ...options, method: 'DELETE' }),
+}
+
+export default helpers;
