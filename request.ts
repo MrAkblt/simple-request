8d772dc7a8164e3612a35695e7c27e5bea876c9b
murat akbulut <m.akblt@live.com>
2024-08-10 21:24:14 +0300

    Update request.ts

--- request.ts
+++ request.ts
@@ -5,20 +5,20 @@ export interface ErrorType<T = any> extends Error {
   response?: ResponseType<T>;
 }
 
-export async function request<T = any>(options: any): Promise<ResponseType<T>> {
+async function request<T = any>(options: any): Promise<ResponseType<T>> {
   if (options.data) {
-    options.headers = { ...options.header, 'Content-Type': 'application/json'}
+		options.headers = { ...options.header, 'Content-Type': 'application/json'}
     options.body = JSON.stringify(options.data);
   }
   let _response = await fetch(options.url, options);
   let response = Object.assign(_response, { data: {} }) as ResponseType<T>
   try {
     const data = await _response.json();
-    response = Object.assign(response, { data })
+    response = Object.assign(response, { data });
   } catch (error) {}
 
   if (!response.ok) {
-    const error: ErrorType<T> = new Error(response.statusText);
+    const error: ErrorType<T> = new Error(JSON.stringify(response.data));
     error.response = response;
     throw error;
   }
@@ -26,12 +26,4 @@ export async function request<T = any>(options: any): Promise<ResponseType<T>> {
   return response;
 }
 
-request.get = <T = any>(url: string, options?: any) => request<T>({ url, ...options });
-request.post = <T = any>(url: string, data: any, options: any = {}) =>
-  request<T>({ url, ...options, data, method: 'POST' });
-
-request.put = <T = any>(url: string, data: any, options: any = {}) =>
-  request<T>({ url, ...options, data, method: 'PUT' });
-
-request.delete = <T = any>(url: string, options: any = {}) =>
-  request<T>({ url, ...options, method: 'DELETE' });
+export default request;
