e18dec25f4fd8f39deba0acb77122672d35dc220
murat akbulut <m.akblt@live.com>
2024-08-11 20:51:24 +0300

    Update macros.ts

--- macros.ts
+++ macros.ts
@@ -3,13 +3,13 @@ import request from "./request";
 const macros = {
 	get: <T = any>(url: string, options?: any) => request<T>({ url, ...options }),
 
-	post: <T = any>(url: string, data: any, options: any = {}) =>
+	post: <T = any>(url: string, data: any, options: any) =>
 		request<T>({ url, ...options, data, method: 'POST' }),
 
-	put: <T = any>(url: string, data: any, options: any = {}) =>
+	put: <T = any>(url: string, data: any, options: any) =>
 		request<T>({ url, ...options, data, method: 'PUT' }),
 
-	delete: <T = any>(url: string, options: any = {}) =>
+	delete: <T = any>(url: string, options?: any) =>
 		request<T>({ url, ...options, method: 'DELETE' }),
 }
 
